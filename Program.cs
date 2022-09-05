3fcdf5076bcb039a55f47b9fae92b9c460947dcc
Karidesz <91680206+Karidesz@users.noreply.github.com>
2022-09-06 00:23:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace x
+{
+    internal class zzzzz
+    {
+        int x = 2;
+
+        string degisken = "zzz";
+
+
+        byte b = 5;      //1byte
+        sbyte c = 5;     //1byte
+
+        short s = 5;     //2byte
+        ushort us = 5;   //2byte
+
+        Int16 i16 = 2;   //2byte
+        int i = 2;       //4byte
+        Int32 i32 = 2;    //4byte
+
+
+
+
+
+        uint ui = 2;    //4byte
+        long l = 2;     //8byte
+        ulong ul = 2;   //16byte
+
+
+        //Reel Sayılar
+        float f = 5;   //4byte
+        double d = 5;   //8byte
+        decimal de = 5;  //16byte
+
+
+        char ch = '2';    //2byte
+        string str = "Alpren"; //sınırsız
+
+
+        bool b1 = true; // false de olabilir
+
+        DateTime dt = DateTime.Now;
+
+
+        object o1 = "x";
+        object o2 = 4.3;
+    }
+    
+}    
+    
